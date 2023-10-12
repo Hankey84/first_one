13d4649f0d81cdaade835dab1bfb0b0bc2962847
Hankey84 <hankeybanister84@gmail.com>
2023-10-12 12:27:29 +0300

    trying to figure out the git and Python

---
+++ hello.py
@@ -0,0 +1,14 @@
+# Извините что не на псевдокоде, мне так проще ;)
+numbers = [2, 5, 13, 7, 6, 4]
+size = len(numbers)
+summ = 0  # чтобы не путать со втроенной функцией sum()
+avg = 0
+index = 0
+
+while index < size:
+    summ += numbers[index]
+    index += 1
+avg = round(summ / size)  # Тоже не люблю вещественные числа
+
+print ('Среднее арифметическое массива = ', avg)
+
